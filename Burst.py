7ad415efa6dad0227290cddadde505c1dddba51d
TomasHjartarson <TomasHjartarson@gmail.com>
2021-12-02 12:25:19 +0000

    add directory_jump

--- Burst.py
+++ Burst.py
@@ -23,3 +23,21 @@ Global_tone_map = True
 Sharpen = True
 Hue = True 
 Saturation = True 
+
+
+### Directory of images
+DIR = r'DIRECTORY DESTINATION'
+
+
+def Process():
+    ##Jump around images
+    for sub_folder in os.listdir(DIR):
+
+        Folder = DIR + "/" + sub_folder
+        for file in os.listdir(Folder):
+            ### DO SOMETHING TO FILE
+            print(file)
+
+
+if __name__ == '__main__':
+    Process()
--- Traditional.py
+++ Traditional.py
@@ -1,3 +1,6 @@
+import os
+import time
+
 ### Script to do the following
 
 ### WHITE BALANCING
@@ -12,6 +15,7 @@
 
 
 
+
 ### Processing
 
 White_balance = True
@@ -19,3 +23,22 @@ Demosaic = True
 Denoise = True 
 Color_space_conversion = True
 Gamma_correction = True  
+
+
+
+### Directory of images
+DIR = r'DIRECTORY DESTINATION'
+
+
+def Process():
+    ##Jump around images
+    for sub_folder in os.listdir(DIR):
+
+        Folder = DIR + "/" + sub_folder
+        for file in os.listdir(Folder):
+            ### DO SOMETHING TO FILE
+            print(file)
+
+
+if __name__ == '__main__':
+    Process()
